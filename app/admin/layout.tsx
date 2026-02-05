cf4d78ca871c11315ef5b2908814955ff422cd78
reshma <reshma24799@gmail.com>
2026-02-05 16:29:09 +0100

    theme toggle in admin dashboard

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,23 +1,25 @@
 import Link from "next/link";
 import { signout } from "@/app/login/actions";
+import { ThemeToggle } from "@/components/ThemeToggle";
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex  min-h-screen ">
             <aside className="w-64 bg-gray-900 text-white">
-                <div className="p-6">
+                <div className="flex space-x-8 p-6">
                     <h2 className="text-lg font-bold">
                         Admin Panel
                     </h2>
+                    <ThemeToggle />
                 </div>
                 <nav className="flex flex-col mt-6 space-y-2 px-4">
-                    <Link href="/admin" className="rounded px-4 py-2 hover:bg-gray-800">
+                    <Link href="/admin" className="rounded px-4 py-4 hover:bg-gray-800">
                         Dashboard
                     </Link>
-                    <Link href="/admin/new" className="rounded px-4 py-2 hover:bg-gray-800">
+                    <Link href="/admin/new" className="rounded px-4 py-4 hover:bg-gray-800">
                         Add new employee
                     </Link>
-                    <Link href="/" className="mt-8 rounded px-4 py-2  hover:bg-gray-800 ">
+                    <Link href="/" className=" rounded px-4 py-4  hover:bg-gray-800 ">
                         Back to Public Site
                     </Link>
                     <form action={signout}>
